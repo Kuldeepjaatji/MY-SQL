e6edf43f5aeb346571fac348d10734265779287b
Kuldeep_jaat_ji <ks7799952@gmail.com>
2025-07-01 18:14:14 +0530

    'updated'

--- First SQL file.sql
+++ First SQL file.sql
@@ -1,2 +1,18 @@
-use Student;
-show databases;
+
+create database Temp2;
+use temp2;
+create table Student(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+);
+describe Student;
+
+
+use temp2;
+create table GUARDIAN(
+Guid CHAR PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+);
+describe Student;
---
+++ second SQL.sql
@@ -0,0 +1,18 @@
+use temp2;
+create table Student(
+Rollnumber INT,
+SName VARCHAR(20),
+SDateOfBrith DATE,
+GUID CHAR(12),
+PRIMARY KEY (Rollnumber));
+describe Student;
+
+
+use temp2;
+create table Student(
+Rollnumber INT,
+SName VARCHAR(20),
+SDateOfBrith DATE,
+GUID CHAR(12),
+PRIMARY KEY (Rollnumber));
+describe Student;
